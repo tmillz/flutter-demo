e4bba7f87c597d671a8892c9954be54e9efeb23e
tmillz <terrymil1981@gmail.com>
2026-08-02 13:43:42 -0400

    chore: update home screen screenshot

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:firebase_auth/firebase_auth.dart';
